21a6ade837a045454939b9f56887282c6eff6fa1
bebekantengahrt17-source <rikoandisetiawan27@gmail.com>
2026-09-16 03:50:56 +0700

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// ISI SETELAH MEMBUAT PROJECT SUPABASE
-window.SUPABASE_URL = "https://xqzwlcqiugspvpgkyke.supabase.co";
-window.SUPABASE_ANON_KEY = "sb_publishable_WHAW-3VN1jXAObMRADequQ_WR70OlfB";
